1a4733033ceed5b35d3298ab03f6e7fddc3426eb
Brice <bricel67@gmail.com>
2022-07-04 17:39:55 +0200

    TodoForm

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+function TodoForm(props) {
+  const [input, setInput] = useState("");
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // props.onSubmit({
+    //   id: Math.floor(Math.random() * 10000),
+    //   text: input,
+    // });
+
+    setInput("");
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="text"
+        id="toto-input"
+        className="todo-input"
+        placeholder="Add a todo"
+        value={input}
+        onChange={handleChange}
+      />
+      <button className="todo-button">Add Todo</button>
+    </form>
+  );
+}
+
+export default TodoForm;
